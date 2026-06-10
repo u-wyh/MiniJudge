9cc9e09791dd70845aefe570b25b010211d7b8a4
wyh <wyh15026582592@outlook.com>
2026-06-10 20:28:44 +0800

    实现多测试点支持

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,31 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // 命令格式：
-    // ./minijudge source.cpp input.txt answer.txt
-    if (argc != 4) {
-        cerr << "Usage: ./minijudge <source.cpp> <input.txt> <answer.txt>" << endl;
+    // 支持两种命令格式：
+    // 1. 单测试点模式：
+    //    ./minijudge source.cpp input.txt answer.txt
+    // 2. 多测试点模式：
+    //    ./minijudge source.cpp problem_dir
+    if (argc != 3 && argc != 4) {
+        cerr << "Usage 1: ./minijudge <source.cpp> <input.txt> <answer.txt>" << endl;
+        cerr << "Usage 2: ./minijudge <source.cpp> <problem_dir>" << endl;
         return 1;
     }
 
     string sourceFile = argv[1];
-    string inputFile = argv[2];
-    string answerFile = argv[3];
+
+    bool multiCaseMode = (argc == 3);
+
+    string inputFile;
+    string answerFile;
+    string problemDir;
+
+    if (multiCaseMode) {
+        problemDir = argv[2];
+    } else {
+        inputFile = argv[2];
+        answerFile = argv[3];
+    }
 
     string tmpDir = "judge_tmp";
 
@@ -43,40 +58,111 @@ int main(int argc, char* argv[]) {
 
     cout << "Compile: OK" << endl;
 
-    // 第二步：运行用户程序
-    int timeUsedMs = 0;
+    if (!multiCaseMode) {
+        // 单测试点模式
+        int timeUsedMs = 0;
 
-    RunResult runResult = runProgram(
-        exeFile,
-        inputFile,
-        outputFile,
-        errorFile,
-        timeLimitMs,
-        timeUsedMs
-    );
+        RunResult runResult = runProgram(
+            exeFile,
+            inputFile,
+            outputFile,
+            errorFile,
+            timeLimitMs,
+            timeUsedMs
+        );
 
-    if (runResult == RunResult::TLE) {
-        cout << "Result: TLE" << endl;
+        if (runResult == RunResult::TLE) {
+            cout << "Result: TLE" << endl;
+            cout << "Time: " << timeUsedMs << " ms" << endl;
+            return 0;
+        }
+
+        if (runResult == RunResult::RE) {
+            cout << "Result: RE" << endl;
+            cout << "Time: " << timeUsedMs << " ms" << endl;
+            return 0;
+        }
+
+        bool accepted = compareFiles(outputFile, answerFile);
+
+        if (accepted) {
+            cout << "Result: AC" << endl;
+        } else {
+            cout << "Result: WA" << endl;
+        }
+
         cout << "Time: " << timeUsedMs << " ms" << endl;
         return 0;
     }
 
-    if (runResult == RunResult::RE) {
-        cout << "Result: RE" << endl;
-        cout << "Time: " << timeUsedMs << " ms" << endl;
+    // 多测试点模式
+    int caseId = 1;
+    int totalTimeMs = 0;
+    bool hasCase = false;
+    string finalResult = "AC";
+
+    while (true) {
+        string caseInputFile = problemDir + "/" + to_string(caseId) + ".in";
+        string caseAnswerFile = problemDir + "/" + to_string(caseId) + ".out";
+
+        if (!filesystem::exists(caseInputFile) || !filesystem::exists(caseAnswerFile)) {
+            break;
+        }
+
+        hasCase = true;
+
+        int timeUsedMs = 0;
+
+        RunResult runResult = runProgram(
+            exeFile,
+            caseInputFile,
+            outputFile,
+            errorFile,
+            timeLimitMs,
+            timeUsedMs
+        );
+
+        totalTimeMs += timeUsedMs;
+
+        cout << "Test " << caseId << ": ";
+
+        if (runResult == RunResult::TLE) {
+            cout << "TLE";
+            finalResult = "TLE";
+            cout << " (" << timeUsedMs << " ms)" << endl;
+            break;
+        }
+
+        if (runResult == RunResult::RE) {
+            cout << "RE";
+            finalResult = "RE";
+            cout << " (" << timeUsedMs << " ms)" << endl;
+            break;
+        }
+
+        bool accepted = compareFiles(outputFile, caseAnswerFile);
+
+        if (accepted) {
+            cout << "AC";
+        } else {
+            cout << "WA";
+            finalResult = "WA";
+            cout << " (" << timeUsedMs << " ms)" << endl;
+            break;
+        }
+
+        cout << " (" << timeUsedMs << " ms)" << endl;
+
+        caseId++;
+    }
+
+    if (!hasCase) {
+        cout << "Result: No test cases found" << endl;
         return 0;
     }
 
-    // 第三步：比较用户输出和标准答案
-    bool accepted = compareFiles(outputFile, answerFile);
-
-    if (accepted) {
-        cout << "Result: AC" << endl;
-    } else {
-        cout << "Result: WA" << endl;
-    }
-
-    cout << "Time: " << timeUsedMs << " ms" << endl;
+    cout << "Final: " << finalResult << endl;
+    cout << "Total Time: " << totalTimeMs << " ms" << endl;
 
     return 0;
 }
